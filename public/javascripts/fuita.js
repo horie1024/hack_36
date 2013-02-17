e902bad5a75507e7f7d20ca50f0ace67fa92bfea
ykawasak <akiu.bullet@gmail.com>
2013-02-17 11:35:25 +0900

    オリジナルの動画再生する処理を追加

--- public/javascripts/fuita.js
+++ public/javascripts/fuita.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
   var socket = io.connect('http://localhost');
   var target = $("#wrapper");
+  var overlay = $("#dialog-overlay");
+  var dialog = $("#dialog");
 
   //stub
-  var data = [{"id":"1", "origin":"http://yahoo.co.jp", "gif":"http://livedoor.blogimg.jp/vsokuvip/imgs/b/1/b122bfe5.gif" }, {"id":"2", "origin":"http://yahoo.co.jp", "gif":"http://livedoor.blogimg.jp/vsokuvip/imgs/1/c/1cc43e81.gif" }]
+  var data = [{"id":"1", "origin":"http://i.gifboom.com/medias/875e3db030c444d092899d4e266c9e5b.mp4", "gif":"http://livedoor.blogimg.jp/vsokuvip/imgs/b/1/b122bfe5.gif" }, {"id":"2", "origin":"http://i.gifboom.com/medias/99d79cd3d2d8428d98eb11d85b5ed3eb.mp4", "gif":"http://livedoor.blogimg.jp/vsokuvip/imgs/1/c/1cc43e81.gif" }]
 
   socket.on('init', function (data) {
     init(data);
@@ -35,7 +37,7 @@ $(document).ready(function(){
       addImg(data[i]);
     };
   }                 
-    
+
   function fuita (data) {
     showDialog();
     console.log("fuitad:" + data);
@@ -53,32 +55,40 @@ $(document).ready(function(){
   function addMovie (data) {
     var id = "#fuita_" + data.id;
     console.log("movie:" + id);
-    $(id).append('<video　src＝”'+ data.origin +' style="display:none"></video>');
+    $(id).append('<div class="videoWrap"  id="origin_'+ data.id 
+      + '" style="display:none"><video src="'+ data.origin 
+      +'" loop preload="auto" autoplay="true"></video></div>');
   }
 
    //gifをDOMに追加する
-   function addImg (img) {
-      console.log(img);
-      var picture = $('<div class="pictWrap" id="fuita_'+ img.id +'"><div class="pict"> <img src="'+ img.gif +'" width="236" height="134"> </div>');
-      target.prepend(picture);
-      picture.click(function(){
-         console.log(img.gif);
+  function addImg (img) {
+    console.log(img);
+    var picture = $('<div class="pictWrap" id="fuita_'+ img.id +'"><div class="pict"> <img src="'+ img.gif +'" width="236" height="134"> </div>');
+    target.prepend(picture);
+    picture.click(function(){
+      console.log(img.gif);
+      var video_id = "#origin_" + img.id;
+      console.log(video_id);
+      overlay.show();
+      $(video_id).show();
+        overlay.click(function () {
+          $(video_id).hide();
+          overlay.hide();
       });
-     //picture.filter(".pictWrap").hoverpulse();
-   }
+    });
+    //picture.filter(".pictWrap").hoverpulse();
+  }
 
-   //吹いたダイアログ表示
-   function showDialog() {
-     console.log("showDialog");
-     var overlay = $("#dialog-overlay");
-     var dialog = $("#dialog");
+  //吹いたダイアログ表示
+  function showDialog() {
+    console.log("showDialog");
 
-     overlay.show();
-     dialog.show();
+    overlay.show();
+    dialog.show();
 
-     setTimeout( function() {
-       overlay.hide();
-       dialog.hide();
-     }, 2000);
-   } 
+    setTimeout( function() {
+      overlay.hide();
+      dialog.hide();
+    }, 2000);
+  } 
 });
